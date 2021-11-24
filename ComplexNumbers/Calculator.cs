ab5fd1184c5b909c2d99ab01698aa26793d829c1
Igor Kind <86460417+ngessda@users.noreply.github.com>
2021-11-24 15:53:32 +0900

    none

---
+++ ComplexNumbers/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    static class Calculator
+    {
+        static private double re1 = Convert.ToDouble(Console.ReadLine());
+        static private double re2 = Convert.ToDouble(Console.ReadLine());
+        static private double im1 = Convert.ToDouble(Console.ReadLine());
+        static private double im2 = Convert.ToDouble(Console.ReadLine());
+        static ComplexNumbers a = new ComplexNumbers(re1, im1);
+        static ComplexNumbers b = new ComplexNumbers(re2, im2);
+        public static void Addition()
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Subtraction()
+        {
+            Console.WriteLine(a - b);
+        }
+        public static void Division()
+        {
+            Console.WriteLine(a / b);
+        }
+        public static void Multiplication()
+        {
+            Console.WriteLine(a * b);
+        }
+    }
+}
---
+++ ComplexNumbers/ComplexNumbers.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    public class ComplexNumbers
+    {
+        private const double EPS = 0.000000001;
+        private double re;
+        private double im;
+        public ComplexNumbers Conj
+        {
+            get
+            {
+                ComplexNumbers c = new ComplexNumbers();
+                c.re = re;
+                c.im = -im;
+                return c;
+            }
+        }
+
+        public ComplexNumbers()
+        {
+            re = 0;
+            im = 0;
+        }
+        public ComplexNumbers(double re1, double im1)
+        {
+            re = re1;
+            im = im1;
+        }
+        public static ComplexNumbers operator+(ComplexNumbers a, ComplexNumbers b)
+        {
+            ComplexNumbers c = new ComplexNumbers();
+            c.re = a.re + b.re;
+            c.im = a.im + b.im;
+            return c;
+        }
+        public static ComplexNumbers operator -(ComplexNumbers a, ComplexNumbers b)
+        {
+            ComplexNumbers c = new ComplexNumbers();
+            c.re = a.re - b.re;
+            c.im = a.im - b.im;
+            return c;
+        }
+        public static ComplexNumbers operator *(ComplexNumbers a, ComplexNumbers b)
+        {
+            ComplexNumbers c = new ComplexNumbers();
+            c.re = (a.re * b.re) - (a.im * b.im);
+            c.im = (a.re * b.im) + (a.im * b.re);
+            return c;
+        }
+        public static ComplexNumbers operator /(ComplexNumbers a, ComplexNumbers b)
+        {
+            try
+            {
+                if(a.re == 0 || a.im == 0 || b.re == 0 || b.im == 0)
+                {
+                    throw new ComplexZeroException();
+                }
+            }
+            catch(ComplexZeroException ex)
+            {
+                throw ex;
+            }
+            ComplexNumbers c = new ComplexNumbers();
+            c.re = ((a.re * b.re) + (a.im * b.im)) / (Math.Pow(b.re, 2) + Math.Pow(b.im, 2));
+            c.im = ((a.im * b.re) - (a.re * b.im)) / (Math.Pow(b.re, 2) + Math.Pow(b.im, 2));
+            return c;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (Math.Abs(re) >= EPS)
+            {
+                if (Math.Abs(im) > EPS)
+                {
+                    sb.Append($"{re} + i * {im}");
+                }
+                else if (Math.Abs(im) < EPS)
+                {
+                    sb.Append($"{re} - i * {-im}");
+                }
+            }
+            else if (Math.Abs(re) < EPS)
+            {
+                if (Math.Abs(im) > EPS)
+                {
+                    sb.Append($" i * {im}");
+                }
+                else if (Math.Abs(im) < EPS)
+                {
+                    sb.Append($"- i * {-im}");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
